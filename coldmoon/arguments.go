860631a47ba4e81a22b9d439833f0b2331fdbd4e
Seeingu <seeingasu@gmail.com>
2024-11-16 21:19:10 +0800

    arguments: createUnmappedArgumentsObject

---
+++ coldmoon/arguments.go
@@ -0,0 +1,37 @@
+package coldmoon
+
+type ArgumentsObject struct {
+	*Object
+}
+
+// 10.4.4.6
+func CreateUnmappedArgumentsObject(agent *Agent, argumentsList []Value) ObjectType {
+	realm := agent.CurrentRealm()
+
+	length := len(argumentsList)
+
+	obj := &ArgumentsObject{
+		Object: NewObject(agent, realm.Intrinsics.ObjectPrototype),
+	}
+
+	obj.DefinePropertyOrThrow(NewStringPropertyKey("length"), &PropertyDescriptor{
+		Value:        NewNumberValue(float64(length)),
+		Writable:     true,
+		Enumerable:   false,
+		Configurable: true,
+	})
+
+	for index, value := range argumentsList {
+		pk := NewIntegerIndexPropertyKey(index)
+		obj.CreateDataPropertyOrThrow(pk, value)
+	}
+
+	obj.DefinePropertyOrThrow(NewStringPropertyKey("callee"), &PropertyDescriptor{
+		Get:          realm.Intrinsics.ThrowTypeError,
+		Set:          realm.Intrinsics.ThrowTypeError,
+		Enumerable:   false,
+		Configurable: false,
+	})
+
+	return obj
+}
