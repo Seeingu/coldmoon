415792348f0f74f93f9ef258dbd1642e3f6f4056
Seeingu <seeingasu@gmail.com>
2024-11-16 20:46:34 +0800

    array: indexOf

--- coldmoon/array.go
+++ coldmoon/array.go
@@ -446,6 +446,41 @@ func NewArrayPrototype(realm *Realm) *ArrayObject {
 		}
 		return FalseValue
 	}
+	var indexOf BehaviorFn = func(this Value, args []Value, newTarget ObjectType) Value {
+		searchElement := args[0]
+		fromIndex := args[1]
+		o := ValueToObject(agent, this)
+		length := o.LengthOfArrayLike()
+		if length == 0 {
+			return NewNumberValue(-1)
+		}
+		n := ToIntegerOrInfinity(agent, fromIndex)
+		if fromIndex == UndefinedValue {
+			Assert(n == 0)
+		}
+		if n == math.Inf(1) {
+			return NewNumberValue(-1)
+		} else if n == math.Inf(-1) {
+			n = 0
+		}
+
+		k := int(math.Max(n, 0))
+		if k < 0 {
+			k = int(math.Max(float64(length)+float64(k), 0))
+		}
+
+		for k < int(length) {
+			kPresent := o.HasProperty(NewIntegerIndexPropertyKey(k))
+			if kPresent {
+				elementK := o.Get(NewIntegerIndexPropertyKey(k))
+				if IsStrictlyEqual(searchElement, elementK) {
+					return NewNumberValue(float64(k))
+				}
+			}
+			k++
+		}
+		return NewNumberValue(-1)
+	}
 
 	DefineBuiltinFunction(object, "join", join, 1, realm)
 	DefineBuiltinFunction(object, "toString", toString, 0, realm)
@@ -455,6 +490,7 @@ func NewArrayPrototype(realm *Realm) *ArrayObject {
 	DefineBuiltinFunction(object, "map", arrayMap, 1, realm)
 	DefineBuiltinFunction(object, "toLocaleString", toLocaleString, 0, realm)
 	DefineBuiltinFunction(object, "includes", includes, 1, realm)
+	DefineBuiltinFunction(object, "indexOf", indexOf, 1, realm)
 
 	var unscopablesValue Value
 	unscopablesList := OrdinaryObjectCreate(agent, nil, nil)
