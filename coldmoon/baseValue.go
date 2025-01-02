87e0204f4969c79074e9aa43c35eb0e7f77adbbd
Seeingu <seeingasu@gmail.com>
2025-01-02 16:15:02 +0800

    refactor(value): simplify BaseValue structure and enhance type handling

--- coldmoon/baseValue.go
+++ coldmoon/baseValue.go
@@ -2,18 +2,17 @@ package coldmoon
 
 type BaseValue struct {
 	Value
-	ref Value
 }
 
 var _ Value = (*BaseValue)(nil)
 
 func (b *BaseValue) ToCompletion() CompletionValue {
-	return NewCompletionValue(b.ref)
+	return NewCompletionValue(b.Value)
 }
 
 func (b *BaseValue) ToPropertyDescriptor() *PropertyDescriptor {
 	return &PropertyDescriptor{
-		Value: b.ref,
+		Value: b.Value,
 		// TODO(C): check writable
 		Writable:     false,
 		Enumerable:   false,
@@ -21,9 +20,36 @@ func (b *BaseValue) ToPropertyDescriptor() *PropertyDescriptor {
 	}
 }
 
+func (b *BaseValue) TypeString() *StringValue {
+	switch v := b.Value.(type) {
+	case *undefinedValue:
+		return NewStringValue("undefined")
+	case *nullValue:
+		return NewStringValue("object")
+	case *BooleanValue:
+		return NewStringValue("boolean")
+	case *NumberValue:
+		return NewStringValue("number")
+	case *StringValue:
+		return NewStringValue("string")
+	case *SymbolValue:
+		return NewStringValue("symbol")
+	case *BigIntValue:
+		return NewStringValue("bigint")
+	case *ObjectValue:
+		if v.Object.InternalMethods().Call != nil {
+			return NewStringValue("function")
+		} else {
+			return NewStringValue("object")
+		}
+	default:
+		panic("unreachable")
+	}
+}
+
 func NewValue(v Value) Value {
 	b := &BaseValue{
-		ref: v,
+		Value: v,
 	}
 	return b
 }
